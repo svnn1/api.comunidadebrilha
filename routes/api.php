b5ad502b9c7f20b8d4cf06458bb021b6d3143352
Silvano Santana <jnkppl@hotmail.com>
2021-06-21 23:33:50 -0300

    - Corrigido o caminho da url

--- routes/api.php
+++ routes/api.php
@@ -24,8 +24,8 @@ Route::group(['prefix' => 'auth'], function () {
   });
 
   Route::group(['prefix' => 'password', 'as' => 'password.'], function () {
-    Route::post('/password/email', [Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('email');
-    Route::post('/password/reset', [Auth\ResetPasswordController::class, 'reset'])->name('reset');
+    Route::post('/email', [Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('email');
+    Route::post('/reset', [Auth\ResetPasswordController::class, 'reset'])->name('reset');
   });
 
   Route::group(['prefix' => 'email', 'as' => 'verification.', 'middleware' => 'auth:api'], function () {
@@ -41,5 +41,27 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['auth:api'
     Route::get('/{userId}', [Admin\UserController::class, 'show'])->name('show');
     Route::match(['patch', 'put'], '/{userId}', [Admin\UserController::class, 'update'])->name('update');
     Route::delete('/{userId}', [Admin\UserController::class, 'destroy'])->name('destroy');
+  });
+});
+
+Route::group(['prefix' => 'blog', 'as' => 'blog.'], function () {
+  Route::group(['prefix' => 'posts', 'as' => 'post.',], function () {
+    Route::get('/', [Admin\Blog\PostController::class, 'index'])->name('index');
+    Route::post('/', [Admin\Blog\PostController::class, 'store'])->name('store');
+    Route::get('/{postId}', [Admin\Blog\PostController::class, 'show'])->name('show');
+    Route::match(['patch', 'put'], '/{postId}', [Admin\Blog\PostController::class, 'update'])->name('update');
+    Route::delete('/{postId}', [Admin\Blog\PostController::class, 'destroy'])->name('destroy');
+
+
+    Route::patch('/{postId}/status', [Admin\Blog\PostController::class, 'updateStatus'])->name('updateStatus');
+  });
+
+  Route::group(['prefix' => 'categories', 'as' => 'category.',], function () {
+    Route::get('/', [Admin\Blog\CategoryController::class, 'index'])->name('index');
+  });
+
+  Route::group(['prefix' => 'tags', 'as' => 'tag.',], function () {
+    Route::get('/', [Admin\Blog\TagController::class, 'index'])->name('index');
+    Route::get('/{tagId}', [Admin\Blog\TagController::class, 'show'])->name('show');
   });
 });
