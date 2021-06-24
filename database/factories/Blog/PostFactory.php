ccbc013c38adad17728411b342f33723abf54148
Silvano Santana <jnkppl@hotmail.com>
2021-06-24 03:19:00 -0300

    - Adicionado todos os states da coluna status

--- database/factories/Blog/PostFactory.php
+++ database/factories/Blog/PostFactory.php
@@ -34,4 +34,89 @@ class PostFactory extends Factory
       'user_id' => User::factory(),
     ];
   }
+
+  /**
+   * Indicate that post is approved.
+   *
+   * @return \Database\Factories\Blog\PostFactory
+   */
+  public function approved(): PostFactory
+  {
+    return $this->state(function (array $attributes) {
+      return [
+        'status' => 'approved',
+      ];
+    });
+  }
+
+  /**
+   * Indicate that post is archived.
+   *
+   * @return \Database\Factories\Blog\PostFactory
+   */
+  public function archived(): PostFactory
+  {
+    return $this->state(function (array $attributes) {
+      return [
+        'status' => 'archived',
+      ];
+    });
+  }
+
+  /**
+   * Indicate that post is drafted.
+   *
+   * @return \Database\Factories\Blog\PostFactory
+   */
+  public function drafted(): PostFactory
+  {
+    return $this->state(function (array $attributes) {
+      return [
+        'status' => 'drafted',
+      ];
+    });
+  }
+
+  /**
+   * Indicate that post is published.
+   *
+   * @return \Database\Factories\Blog\PostFactory
+   */
+  public function published(): PostFactory
+  {
+    return $this->state(function (array $attributes) {
+      return [
+        'status'       => 'published',
+        'published_at' => now(),
+      ];
+    });
+  }
+
+  /**
+   * Indicate that post is rejected.
+   *
+   * @return \Database\Factories\Blog\PostFactory
+   */
+  public function rejected(): PostFactory
+  {
+    return $this->state(function (array $attributes) {
+      return [
+        'status' => 'rejected',
+      ];
+    });
+  }
+
+  /**
+   * Indicate that post is submitted.
+   *
+   * @return \Database\Factories\Blog\PostFactory
+   */
+  public function submitted(): PostFactory
+  {
+    return $this->state(function (array $attributes) {
+      return [
+        'status' => 'submitted',
+      ];
+    });
+  }
 }
