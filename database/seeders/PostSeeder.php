7116b5a7bb21b894c8d84b01eb05e278765c3a33
Silvano Santana <jnkppl@hotmail.com>
2021-07-02 04:26:44 -0300

    - Added
    Adicionado todos os status dos posts no seeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -22,9 +22,45 @@ class PostSeeder extends Seeder
   public function run()
   {
     Post::factory()
-      ->count(30)
+      ->count(5)
+      ->approved()
       ->has(Image::factory(), 'cover')
-      ->has(Tag::factory()->count(5), 'tags')
+      ->has(Tag::factory()->count(1), 'tags')
+      ->create();
+
+    Post::factory()
+      ->count(2)
+      ->archived()
+      ->has(Image::factory(), 'cover')
+      ->has(Tag::factory()->count(1), 'tags')
+      ->create();
+
+    Post::factory()
+      ->count(2)
+      ->drafted()
+      ->has(Image::factory(), 'cover')
+      ->has(Tag::factory()->count(1), 'tags')
+      ->create();
+
+    Post::factory()
+      ->count(2)
+      ->rejected()
+      ->has(Image::factory(), 'cover')
+      ->has(Tag::factory()->count(1), 'tags')
+      ->create();
+
+    Post::factory()
+      ->count(20)
+      ->published()
+      ->has(Image::factory(), 'cover')
+      ->has(Tag::factory()->count(1), 'tags')
+      ->create();
+
+    Post::factory()
+      ->count(2)
+      ->submitted()
+      ->has(Image::factory(), 'cover')
+      ->has(Tag::factory()->count(1), 'tags')
       ->create();
   }
 }
